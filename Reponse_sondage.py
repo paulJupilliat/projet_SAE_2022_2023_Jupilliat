8f20395cb22a6d106ea17aeba0079b4254fe5c0b
Mathys-Lemort <lemormath@gmail.com>
2022-10-26 13:21:41 +0200

    implementation python 3 classes

--- Reponse_sondage.py
+++ Reponse_sondage.py
@@ -1,7 +1,45 @@
-class Reponse_sondage(models.Model):
-    _name = 'reponse.sondage'
-    _description = 'Reponse_sondage'
-
-    name = fields.Char(string='Nom', required=True)
-    reponse = fields.Char(string='Reponse', required=True)
-    sondage_id = fields.Many2one('sondage', string='Sondage')
+import Oraux
+import Eleve
+class Reponse_sondage:
+    def __init__(self, participation, matiere, commentaire, dateSondage, Eleve, idSondage, Oraux):
+        self.participation = participation
+        self.matiere = matiere
+        self.commentaire = commentaire
+        self.dateSondage = dateSondage
+        self.Eleve = Eleve
+        self.idSondage = idSondage
+        self.Oraux = Oraux
+        
+    def __repr__(self) -> str:
+        return "Participation : "+str(self.participation)+" Matiere : "+str(self.matiere)+" Commentaire : "+str(self.commentaire)+" Date : "+str(self.dateSondage)+" Eleve : "+str(self.Eleve)+" idSondage : "+str(self.idSondage)+" Oraux : "+str(self.Oraux)
+    
+    def getParticipation(self):
+        return self.participation
+    
+    def getMatiere(self):
+        return self.matiere
+    def getCommentaire(self):
+        return self.commentaire
+    def getDateSondage(self):
+        return self.dateSondage
+    def getEleve(self):
+        return self.Eleve
+    def getIdSondage(self):
+        return self.idSondage
+    def getOraux(self):
+        return self.Oraux
+    def setParticipation(self,participation):
+        self.participation=participation
+    def setMatiere(self,matiere):
+        self.matiere=matiere
+    def setCommentaire(self,commentaire):
+        self.commentaire=commentaire
+    def setDateSondage(self,dateSondage):
+        self.dateSondage=dateSondage
+    def setEleve(self,eleve):
+        self.Eleve=eleve
+    def setIdSondage(self,idSondage):
+        self.idSondage=idSondage
+    def setOraux(self,oraux):
+        self.Oraux=oraux
+    
