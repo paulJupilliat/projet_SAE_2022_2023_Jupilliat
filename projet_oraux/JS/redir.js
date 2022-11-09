72870d4ecf615730ab4c9986ec5ea116c8d320a9
ChloeLidec <chloe.lidec@etu.univ-orleans.fr>
2022-11-09 20:41:44 +0100

    parametres css reglé et nav adapté + chgt images nav

--- projet_oraux/JS/redir.js
+++ projet_oraux/JS/redir.js
@@ -1,7 +1,6 @@
 function redirection(id,dir){
     const urlact=window.location.href;
     const nom=urlact.split("/").pop();
-    console.log(nom);
     const url=urlact.replace(nom,dir+".html");
     if (nom=="connexionAdm.html" || nom=="connexionProf.html"){
     document.cookie=id;}
