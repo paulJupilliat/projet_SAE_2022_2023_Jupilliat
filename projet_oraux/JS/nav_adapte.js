21addbcfee1b0c261e1df15224626f2d91b64bfa
ChloeLidec <chloe.lidec@etu.univ-orleans.fr>
2022-11-09 21:02:48 +0100

    soutien sans js et nav sticky

--- projet_oraux/JS/nav_adapte.js
+++ projet_oraux/JS/nav_adapte.js
@@ -21,11 +21,11 @@ function change_nav(){
         '<div class="main_pages">'+
            ' <a href="./Acceuil.html" id="navbar_home">'+
             '<img src="'+img_home+'" height="50em" alt="" height="60em"></a>'+
-            '<a href="./ResQCM.html" class="navbar__links">Resultats QCM</a>'+
+            '<a href="./ResQCM.html" class="navbar__links">Résultats QCM</a>'+
             '<a href="./res_sond.html" class="navbar__links">Sondages</a>'+
-            '<a href="./Dispo.html" class="navbar__links">Disponibilité Enseignant</a>'+
-            '<a href="/" class="navbar__links">Gerer ses disponibilités</a>'+
-            '<a href="/" class="navbar__links">Voir le soutien</a>'+
+            '<a href="./Dispo.html" class="navbar__links">Disponibilités enseignants</a>'+
+            '<a href="/" class="navbar__links">Gérer ses disponibilités</a>'+
+            '<a href="./Soutien.html" class="navbar__links">Voir le soutien</a>'+
             '<a href="./SuiviGenEtu.html" class="navbar__links">Suivi Etudiant</a>'+
             '<a href="./paramEns.html" class="navbar__links"><img src="'+img_param+'" height="50em" alt=""></a>'+
             '<a href="./index.html" class="navbar__links"><img src="../img/deconnexion.png" height="40em" alt=""></a></div>'
@@ -33,10 +33,10 @@ function change_nav(){
     else{
         nav.innerHTML='<label for="toggle" class="label_nav">☰</label><input type="checkbox" id="toggle"><div class="main_pages">'+
             '<a href="./Acceuil.html" id="navbar_home"><img src="'+img_home+'" height="50em" alt="" height="60em"></a>'+
-            '<a href="./ResQCM.html" class="navbar__links">Resultats QCM</a>'+
+            '<a href="./ResQCM.html" class="navbar__links">Résultats QCM</a>'+
             '<a href="./res_sond.html" class="navbar__links">Sondages</a>'+
-            '<a href="./Dispo.html" class="navbar__links">Disponibilité Enseignant</a>'+
-            '<a href="/" class="navbar__links">Voir le soutien</a>'+
+            '<a href="./Dispo.html" class="navbar__links">Disponibilités enseignants</a>'+
+            '<a href="./Soutien.html" class="navbar__links">Voir le soutien</a>'+
             '<a href="./SuiviGenEtu.html" class="navbar__links">Suivi Etudiant</a>'+
             '<a href="./paramAdm.html" class="navbar__links"> <img src="'+img_param+'" height="50em" alt=""></a>'+
             '<a href="./index.html" class="navbar__links"><img src="../img/deconnexion.png" height="40em" alt=""></a></div>'}
