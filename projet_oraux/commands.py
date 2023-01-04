62201e1eaec465a0b15ff5ced288f8eacf2ea0e0
ChloeLidec <chloe.lidec@etu.univ-orleans.fr>
2023-01-04 12:57:17 +0100

    ajouts gestion param default

--- projet_oraux/commands.py
+++ projet_oraux/commands.py
@@ -36,3 +36,39 @@ def passwd(username, password):
     u = User.query.get(username)
     u.password = m.hexdigest()
     db.session.commit()
+
+def lecture_parametre_def(id_recherche):
+    """Lis le fichier par_def.txt et retourne
+    la valeur correspondant à l'id recherché
+    Si l'id n'est pas trouvé, retourne None
+    Args:
+        id_recherche : id du paramètre à rechercher
+    """
+    fichier = open("../static/donnees/par_def.txt", "r")
+    for ligne in fichier:
+        if ligne[0] != "#":
+            id, valeur = ligne.split(":")
+            if id == id_recherche:
+                fichier.close()
+                return valeur.strip()
+    fichier.close()
+    return None
+
+def ecriture_parametre_def(id, valeur):
+    """Ecrit dans le fichier par_def.txt la valeur
+    correspondant à l'id recherché
+    Args:    
+        id : id du paramètre à modifier
+        valeur : valeur à écrire"""
+    fichier = open("../static/donnees/par_def.txt", "r")
+    lignes = fichier.readlines()
+    fichier.close()
+    fichier = open("../static/donnees/par_def.txt", "w")
+    for ligne in lignes:
+        if ligne[0] != "#":
+            id_ligne, _ = ligne.split(":")
+            if id_ligne == id:
+                fichier.write(id + ":" + valeur)
+        else:
+            fichier.write(ligne)
+    fichier.close()
