7bc98a07731260aaa89007fe3dd868eae75c7d6e
paulJupilliat <jupilliatp@gmail.com>
2023-01-06 10:59:35 +0100

    css sondage MAJ

--- projet_oraux/views.py
+++ projet_oraux/views.py
@@ -186,8 +186,8 @@ def Disponibilite():
     {"id_semaine":4,"date_debut":"23/01/2023","date_fin":"29/01/2023"}, {"id_semaine":5,"date_debut":"30/01/2023","date_fin":"05/02/2023"},
     {"id_semaine":6,"date_debut":"06/02/2023","date_fin":"12/02/2023"},{"id_semaine":7,"date_debut":"13/02/2023","date_fin":"19/02/2023"},
     {"id_semaine":8,"date_debut":"20/02/2023","date_fin":"26/02/2023"},{"id_semaine":9,"date_debut":"27/02/2023","date_fin":"05/03/2023"}]
-    matieres=["Python","Java","C++"]
-    possibles={"Chabin":["BDD","Java","Reseau"],"Adobet":["IHM","Java","BDD"],"Arsouze":["Web","Python","Dev Efficace"]}
+    matieres=["Python","Java","C++", "BDD", "Reseau", "IHM", "Web", "Dev Efficace"]
+    possibles={"Chabin":["BDD","Java","Reseau"],"Adobet":["IHM","Java","BDD"],"Arsouze":["Web","Python","Dev Efficace"], "Anglade" : ["Python",], "Roza":["Dev Efficace", "Java", "Python"]}
     dispos = [{"nom_prof": "Chabin", "date_oral":"02/01/2023"}, {"nom_prof": "Arzouz","date_oral":"02/01/2023"}]
     return render_template("Dispo.html",title="Disponibilité", admin=False, semaines=semaines, dispos = dispos, matieres=matieres, possibles= possibles)
 
