c06f25f56cb142a947ba373ae7b79c140a2d97cc
paulJupilliat <jupilliatp@gmail.com>
2023-02-27 09:46:39 +0100

    tout les triggers fonctionnent

--- BD/test_insert.sql
+++ BD/test_insert.sql
@@ -1,3 +1,4 @@
+-- Active: 1677486538660@@127.0.0.1@3306@soutien
 insert into SONDAGE values(1,'url1');
 
 insert into MATIERE values(100, "maths");
@@ -12,10 +13,12 @@ insert into ELEVE values(3,"manach", "erwan",12,13);
 insert into PROF values(10, "arzouz","julien","juju@gmail.com" );
 insert into PROF values(11, "ADOBET","CHRISTINE","j@gmail.com" );
 
+insert into PROF VALUES(12,"ESPINAS","Guilaine","G@gmail.com" );
+
  
 
 
-
+insert into SOUTENIR VALUES(12, 100);
 insert into SOUTENIR values (10,100);
 insert into SOUTENIR values (10,150);
 -- trigger2
@@ -28,11 +31,11 @@ insert into SOUTENIR values (11,100);
 insert into ORAUX values (2,"oral2",4,100,10,STR_TO_DATE('2022/10/22','%Y/%m/%d'));
 insert into ORAUX values(1,"oral1",2,150,10,STR_TO_DATE('2022/10/22','%Y/%m/%d'));
 -- trigger2
-insert into ORAUX values(3,"oral3",5,150,NULL,STR_TO_DATE('2022/10/25','%Y/%m/%d'));
+insert into ORAUX values(3,"oral3",5,150,12,STR_TO_DATE('2022/10/25','%Y/%m/%d'));
 -- sencé retourner le pb car le prof 10 n'est pas capable de faire le cours 150
 
 -- trigger3
-insert into ORAUX values(5,"ORAL5",3,100,NULL,STR_TO_DATE('2022/10/20','%Y/%m/%d'));
+insert into ORAUX values(5,"ORAL5",3,100,12,STR_TO_DATE('2022/10/20','%Y/%m/%d'));
 -- FAIRE DES UPDATES SUR LES ORAUX SQLACHEMY POUR AJOUTER DES PROFESSEURS AUX ORAUX
 
 insert into EST_DISPONIBLE values(10,1);
@@ -42,7 +45,7 @@ insert into EST_DISPONIBLE values(11,2);
 
 insert into RESULTAT VALUES (10.5,2,1);
 
-insert into REPSONDAGE values("oui", "maths", "j'aime bien les maths mais j'ai du mal",STR_TO_DATE('2022/10/21','%Y/%m/%d'),1,1,1);
+insert into REPSONDAGE values("oui", "maths", "j'aime bien les maths mais j'ai du mal",STR_TO_DATE('2022/10/21','%Y/%m/%d'),1,1);
 
 insert into PARTICIPE values("paul s'améliore pas dutout en maths ce bouffon", 1, 2);
 insert into PARTICIPE values("maxime s'améliore en maths", 2, 2);
