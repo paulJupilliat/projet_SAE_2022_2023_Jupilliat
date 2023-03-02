812058c3078c2602a89d73c69f8d56e7d9aff619
Mathys-Lemort <lemormath@gmail.com>
2023-03-02 09:25:42 +0100

    récupération des daes grâce a selenium

---
+++ drop.sql
@@ -0,0 +1,16 @@
+drop table reponsequestionsondage;
+drop table estdisponible;
+drop table possibilitesoutien;
+drop table semaine;
+drop table periode;
+drop table repsondage;
+drop table resultatqcm;
+drop table participantsoral;
+drop table oral;
+drop table user;
+drop table professeur;
+drop table qcm;
+drop table matiere;
+drop table questionsondage;
+drop table sondage;
+drop table eleve;
